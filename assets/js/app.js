1575eb8e5e58b152367698f1d0bf839c06275746
Jonathan Silva <84137692+JonathSilva@users.noreply.github.com>
2021-05-25 19:09:06 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,13 @@
-const sorteio = () => {
-   
+let button = document.getElementById('btn');
+
+// Evento para sortear o número aleatório
+
+button.addEventListener("click", () => {
+
     let np = document.getElementById("number").value;
 
     let ns = Math.floor(Math.random() * np + 1);
 
     document.getElementById("result").innerHTML = ns
    
-}
+});
